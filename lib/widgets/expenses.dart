99bf672fef4f4fa9f1c35588a613f8cfd75a37ed
Rashad Alatabany <rashad.atabanyy@gmail.com>
2026-01-02 06:18:25 +0200

    more changes in the code to lec 112

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -25,11 +26,35 @@ class _ExpensesState extends State<Expenses> {
       amount: 15.00,
       date: DateTime.now(),
     ),
+    Expense(
+      category: Category.travel,
+      title: 'Trip to Paris',
+      amount: 15.00,
+      date: DateTime.now(),
+    ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense()
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Expense Tracker',
+        ),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('Hello Expenses!'),
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
 
 class ExpenseItem extends StatelessWidget {
   const ExpenseItem(this.expense, {super.key});
@@ -17,20 +17,16 @@ class ExpenseItem extends StatelessWidget {
             const SizedBox(height: 4),
             Row(
               children: [
-                Text('${expense.amount.toStringAsFixed(2)} \$'),
+                Text('${expense.amount.toStringAsFixed(2)}\$\$'),
                 const Spacer(),
                 Row(
                   children: [
                     Text(
                       categoryIcons[expense.category]!,
-                      style: const TextStyle(fontSize: 24),
+                      style: const TextStyle(fontSize: 20),
                     ),
-
-                    // Icon(categoryIcons[expense.category),
-                    const SizedBox(width: 8),
-                    Text(
-                      expense.formattedDate,
-                      ),
+                    const SizedBox(width: 10),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
